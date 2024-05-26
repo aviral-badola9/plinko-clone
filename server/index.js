a2f358bf7d15e89cbd2f81a04ed599adaa165fe5
Aviral Badola <aviralrox.badola5@gmail.com>
2024-05-26 16:28:39 +0530

    Initial frontend setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const routes = require('./routes');
 
+app.use(cors);
 app.use('/api', routes);
 
 app.use(express.json());
